b50d7d5598e12238e94c90b5c3e34e7e588571a7
Ayoma Wijethunga <ayoma@wso2.com>
2019-01-23 08:24:13 +0530

    Move encoding funcs from internal and stringlib

--- src/main/java/org/ballerinalang/stdlib/encoding/nativeimpl/ByteArrayToString.java
+++ src/main/java/org/ballerinalang/stdlib/encoding/nativeimpl/ByteArrayToString.java
@@ -1,17 +1,17 @@
 /*
- * Copyright (c) 2019, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
  * WSO2 Inc. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License.
  * You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the
+ * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
  */
@@ -21,41 +21,41 @@ package org.ballerinalang.stdlib.encoding.nativeimpl;
 import org.ballerinalang.bre.Context;
 import org.ballerinalang.bre.bvm.BlockingNativeCallableUnit;
 import org.ballerinalang.model.types.TypeKind;
+import org.ballerinalang.model.values.BString;
 import org.ballerinalang.model.values.BValueArray;
 import org.ballerinalang.natives.annotations.Argument;
 import org.ballerinalang.natives.annotations.BallerinaFunction;
 import org.ballerinalang.natives.annotations.ReturnType;
-import org.ballerinalang.stdlib.encoding.Constants;
-import org.ballerinalang.stdlib.encoding.EncodingUtil;
+import org.ballerinalang.util.exceptions.BallerinaException;
+
+import java.io.UnsupportedEncodingException;
 
 /**
- * Extern function ballerina.encoding:decodeHex.
+ * Convert byte array to string.
  *
- * @since 0.991.0
+ * @since 0.980
  */
 @BallerinaFunction(
         orgName = "ballerina", packageName = "encoding",
-        functionName = "decodeHex",
+        functionName = "byteArrayToString",
         args = {
-                @Argument(name = "input", type = TypeKind.STRING)
+                @Argument(name = "content", type = TypeKind.ARRAY, elementType = TypeKind.BYTE),
+                @Argument(name = "encoding", type = TypeKind.STRING)
         },
-        returnType = {
-                @ReturnType(type = TypeKind.ARRAY, elementType = TypeKind.BYTE),
-                @ReturnType(type = TypeKind.RECORD, structType = Constants.ENCODING_ERROR,
-                        structPackage = Constants.ENCODING_PACKAGE)
-        },
+        returnType = {@ReturnType(type = TypeKind.STRING)},
         isPublic = true
 )
-public class DecodeHex extends BlockingNativeCallableUnit {
+public class ByteArrayToString extends BlockingNativeCallableUnit {
 
     @Override
     public void execute(Context context) {
-        String input = context.getStringArgument(0);
+        byte[] bytes = ((BValueArray) context.getRefArgument(0)).getBytes();
+        String encoding = context.getStringArgument(0);
         try {
-            byte[] output = EncodingUtil.decodeHex(input);
-            context.setReturnValues(new BValueArray(output));
-        } catch (IllegalArgumentException e) {
-            context.setReturnValues(EncodingUtil.createEncodingError(context, "input is not a valid Hex value"));
+            String value = new String(bytes, encoding);
+            context.setReturnValues(new BString(value));
+        } catch (UnsupportedEncodingException e) {
+            throw new BallerinaException("unsupported encoding: " + encoding , e);
         }
     }
 }
